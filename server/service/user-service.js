44f700015dbaaa210d02d57924c3fd3c2afcb577
dmm <dmvshn@gmail.com>
2021-11-16 13:11:52 +0300

    2

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,17 +1,17 @@
-const UserModel = require("../models/user-model")
-const bcrypt = require("bcrypt")
-const uuid = require("uuid")
-const mailService = require("./mail-service")
-const tokenService = require("./token-service")
-const UserDto = require("../dtos/user-dto")
-const ApiError = require("../exceptions/api-error")
+const UserModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserDto = require('../dtos/user-dto');
+const ApiError = require('../exceptions/api-error');
 
 class UserService {
     async registration(email, password) {
         const candidate = await UserModel.findOne({email})
         if (candidate) {
-            throw ApiError.BadRequest(`user with ${email} exists`)
-
+            console.log(candidate)
+            throw ApiError.BadRequest(`Пользователь с почтовым адресом ${email} уже существует`)
         }
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4(); // v34fa-asfasf-142saf-sa-asf
@@ -29,54 +29,54 @@ class UserService {
     async activate(activationLink) {
         const user = await UserModel.findOne({activationLink})
         if (!user) {
-            throw ApiError.BadRequest("activate ")
+            throw ApiError.BadRequest('Неккоректная ссылка активации')
         }
-        user.isActivated = true
-        await user.save()
+        user.isActivated = true;
+        await user.save();
     }
 
     async login(email, password) {
         const user = await UserModel.findOne({email})
         if (!user) {
-            throw ApiError.BadRequest('no user with this email')
+            throw ApiError.BadRequest('Пользователь с таким email не найден')
         }
-        const isPassEquals = await bcrypt.compare(password, user.password)
+        const isPassEquals = await bcrypt.compare(password, user.password);
         if (!isPassEquals) {
-            throw ApiError.BadRequest("incorrect password")
+            throw ApiError.BadRequest('Неверный пароль');
         }
-        const userDto = new UserDto(user)
-        const tokens = tokenService.generateTokens({...userDto})
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
 
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
         return {...tokens, user: userDto}
     }
 
     async logout(refreshToken) {
-        const token = await tokenService.removeToken(refreshToken)
-        return token
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
     }
 
     async refresh(refreshToken) {
         if (!refreshToken) {
-            throw ApiError.UnauthorizedError()
+            throw ApiError.UnauthorizedError();
         }
-        const userData = tokenService.validateRefreshToken(refreshToken)
-        const tokenFromDb = await tokenService.findToken(refreshToken)
+        const userData = tokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await tokenService.findToken(refreshToken);
         if (!userData || !tokenFromDb) {
-            throw ApiError.UnauthorizedError()
+            throw ApiError.UnauthorizedError();
         }
-        const user = await UserModel.findById(userData.id)
-        const userDto = new UserDto(user)
-        const tokens = tokenService.generateTokens({...userDto})
+        const user = await UserModel.findById(userData.id);
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
 
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
         return {...tokens, user: userDto}
     }
 
-    async getAllUsers(){
-        const users = await UserModel.find()
-        return users
+    async getAllUsers() {
+        const users = await UserModel.find();
+        return users;
     }
 }
 
-module.exports = new UserService()
+module.exports = new UserService();
